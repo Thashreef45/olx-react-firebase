a3c1e22bdeefb29bad819d598d7a59b40bf76fbb
Thashreef45 <ahmedthashreef@gmail.com>
2023-06-01 18:29:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useEffect, useContext } from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Signup from './Pages/Signup'
+import Login from './Pages/Login'
+import Home from './Pages/Home';
+import View from './Pages/ViewPost'
+import Create from './Pages/Create'
+import Context, { FirebaseContext } from './store/Context';
+import { AuthContext } from './store/Context';
+import Post from './store/PostContext';
 
 function App() {
+  const { setUser } = useContext(AuthContext)
+  const { firebase } = useContext(FirebaseContext)
+  useEffect(() => {
+    // console.log(user,'<<user auth here')
+    firebase.auth().onAuthStateChanged((user) => {
+      setUser(user)
+    })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Post>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/signup' element={<Signup />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/create' element={<Create />}></Route>
+          <Route path='/view' element={<View />}></Route>
+
+        </Routes>
+      </Post>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {FirebaseContext} from './store/Context'
+import firebase from './firebase/config'
+import Context from './store/Context';
+import {BrowserRouter} from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+<BrowserRouter>
+    <FirebaseContext.Provider value={{firebase}}>
+        <Context>
+            <App />
+        </Context>
+    </FirebaseContext.Provider>
+</BrowserRouter>
+,document.getElementById('root'));
